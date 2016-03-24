1e1d231175db2b66adbaf9d037d15cd8547696ae
eagerNewb <kk.manoilov@gmail.com>
2016-03-24 14:12:45 +0200

    Initial commit, fetches profile photo and repository

---
+++ README.md
@@ -0,0 +1,2 @@
+Playing around with github api and pure javascript. Hope i can make it into a
+full github profile fetcher.
---
+++ javascripts/application.js
@@ -0,0 +1,65 @@
+function init() {
+	
+	// fire-up the ajax call
+	function xhr() {
+		function requestListener() {
+			var req = JSON.parse(this.responseText);
+			
+
+			for(var i=0;i<req.length;i++){
+		        var obj = req[i];
+
+		        var reposList = document.getElementById("reposList");
+		        reposList.innerHTML += "<li class='list-group-item'>" + obj.name.toString() + "</li>";
+
+		    }
+
+
+		}
+		function requestListener2() {
+			var req = JSON.parse(this.responseText);
+			
+			var avatar = req.avatar_url;
+			document.getElementById('githubAvatar').src = avatar;
+
+			var username = req.name;
+			document.getElementById('githubName').innerHTML = username.toString();
+
+			document.getElementById('githubName').src.reset();
+			document.getElementById('githubAvatar').src.reset();
+		}
+
+		function failedTransfer(e) {
+
+		    document.getElementById("error").innerHTML = "Enter existing username";
+		}
+		// grab user input
+		var userid = document.getElementById('user').value;
+
+		// make new xmlhttp object
+		var xmlhttpRepos = new XMLHttpRequest();
+		var xmlhttpAvatar = new XMLHttpRequest();
+
+		// request url
+		var urlRepos = 'https://api.github.com/users/'+userid+'/repos';
+		var urlAvatar = 'https://api.github.com/users/'+userid;
+
+		// add event listener
+		xmlhttpRepos.addEventListener('load', requestListener);
+		xmlhttpRepos.addEventListener('error', requestListener);
+
+		xmlhttpAvatar.addEventListener('load', requestListener2);
+		xmlhttpAvatar.addEventListener('error', failedTransfer);
+
+		// open the request
+		xmlhttpRepos.open('GET', urlRepos, true);
+		xmlhttpAvatar.open('GET', urlAvatar, true);
+	
+		// send
+	    xmlhttpRepos.send();
+		xmlhttpAvatar.send();
+
+	}
+
+	xhr();
+}
